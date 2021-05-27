823aff769857f56bc6eb3548474e949e52fe79e4
Ajaya1000 <ajaymati1000@gmail.com>
2021-05-27 13:04:25 +0530

    changed header functional component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react'
-import '../scss/header.scss'
-export default class Header extends Component {
-  render() {
-    return (
-      <>
-        <nav className="navbar">
-          <div className="navbar-items">
-            <a className="navbar-item" href="#home">
-              Home
-            </a>
-            <a className="navbar-item" href="#works">
-              {" "}
-              Work{" "}
-            </a>
-            <a className="navbar-item" href="#contact">
-              {" "}
-              Contact{" "}
-            </a>
-          </div>
-          <div className="navbar-items">
-            <a
-              className="navbar-item"
-              href="https://www.facebook.com/profile.php?id=100010825184962"
-            >
-              <i className="fa fa-facebook fa-lg"></i>
-            </a>
-            <a className="navbar-item" href="https://twitter.com/MatiAjay">
-              <i className="fa fa-twitter fa-lg"></i>
-            </a>
-            <a className="navbar-item" href="https://github.com/Ajaya1000/">
-              <i className="fa fa-github fa-lg"></i>
-            </a>
-            <a
-              className="navbar-item"
-              href="https://www.linkedin.com/in/ajaya-mati-252a85184/"
-            >
-              <i className="fa fa-linkedin fa-lg"></i>
-            </a>
-          </div>
-        </nav>
-      </>
-    );
-  }
-}
+import React from 'react';
+
+import '../scss/header.scss';
+
+const Header = () => (
+  <>
+    <nav className='navbar'>
+      <div className='navbar-items'>
+        <a className='navbar-item' href='#home'>
+          Home
+        </a>
+        <a className='navbar-item' href='#works'>
+          Work
+        </a>
+        <a className='navbar-item' href='#contact'>
+          Contact
+        </a>
+      </div>
+      <div className='navbar-items'>
+        <a
+          className='navbar-item'
+          href='https://www.facebook.com/profile.php?id=100010825184962'
+        >
+          <i className='fa fa-facebook fa-lg'></i>
+        </a>
+        <a className='navbar-item' href='https://twitter.com/MatiAjay'>
+          <i className='fa fa-twitter fa-lg'></i>
+        </a>
+        <a className='navbar-item' href='https://github.com/Ajaya1000/'>
+          <i className='fa fa-github fa-lg'></i>
+        </a>
+        <a
+          className='navbar-item'
+          href='https://www.linkedin.com/in/ajaya-mati-252a85184/'
+        >
+          <i className='fa fa-linkedin fa-lg'></i>
+        </a>
+      </div>
+    </nav>
+  </>
+);
+
+export default Header;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react'
-import avatar from '../assests/avatar.svg'
-import footsvg from '../assests/hero.svg'
+import React from 'react';
+import avatar from '../assests/avatar.svg';
+import footsvg from '../assests/hero.svg';
 
-import '../scss/info.scss'
-export default class Home extends Component {
-  render() {
-    return (
-      <>
-        <section className='info' id='home'>
-            <div className='info-body'>
-                <h1>
-                    Front-end Developer & Competitive coder
-                </h1>
-                <h2>
-                    I code beautifully and love to solve practical problems.
-                </h2>
-                <img src={avatar} alt='avatar'/>
-            </div>
-            < div className = 'info-footer' >
-                <img src={footsvg} alt='footer' />
-            </div>
-        </section>
-      </>
-    )
-  }
-}
+import '../scss/info.scss';
+const Home = () => (
+  <>
+    <section className='info' id='home'>
+      <div className='info-body'>
+        <h1>Front-end Developer & Competitive coder</h1>
+        <h2>I code beautifully and love to solve practical problems.</h2>
+        <img src={avatar} alt='avatar' />
+      </div>
+      <div className='info-footer'>
+        <img src={footsvg} alt='footer' />
+      </div>
+    </section>
+  </>
+);
+export default Home;
