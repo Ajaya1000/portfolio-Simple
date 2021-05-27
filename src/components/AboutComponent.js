81d1dd1a46123638cc89331608ade8ae65a6844b
Ajaya1000 <ajaymati1000@gmail.com>
2021-05-27 12:59:02 +0530

    changed about to functional component

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react'
-import '../scss/about.scss'
-export default class About extends Component {
-  render() {
-    return (
-      <>
-        <section className='about' >
-            <div className='container'>
-                <h1>Hii, I'm Ajay. Nice to meet you.</h1>
-                < h2 > Since beginning my journey as a competitive coder one year ago, I've gained a lot of expirience in practical problem solving. Then I dived into 
-                web developing as it make me curious to work on interface with which I can help other with my problem solving ability. I'm quietly confident, naturally curious, and perpetually working on improving my problem solving ability at a time. </h2>
-            </div>
-        </section>
-      </>
-    )
-  }
-}
+import React from 'react';
+
+import '../scss/about.scss';
+
+const About = () => (
+  <>
+    <section className='about'>
+      <div className='container'>
+        <h1>Hii, I'm Ajay. Nice to meet you.</h1>
+        <h2>
+          Since beginning my journey as a competitive coder one year ago, I've
+          gained a lot of expirience in practical problem solving. Then I dived
+          into web developing as it make me curious to work on interface with
+          which I can help other with my problem solving ability. I'm quietly
+          confident, naturally curious, and perpetually working on improving my
+          problem solving ability at a time.{' '}
+        </h2>
+      </div>
+    </section>
+  </>
+);
+export default About;
